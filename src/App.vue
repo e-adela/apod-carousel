0773fadcd94340fd728c0fe8c8388cfbe76b7511
emadadel999 <emadadel1234@gmail.com>
2024-03-02 23:21:42 -0500

    some ui fixes

--- src/App.vue
+++ src/App.vue
@@ -68,7 +68,7 @@ interface iAPOD {
 
 const breakpoints = [
   {
-    size: 1426,
+    size: 1600,
     itemsToShow: 6,
   },
   {
@@ -76,7 +76,7 @@ const breakpoints = [
     itemsToShow: 5,
   },
   {
-    size: 400,
+    size: 486,
     itemsToShow: 4,
   },
   {
@@ -105,7 +105,7 @@ onMounted(async () => {
     );
     let res = await apods.json() as iAPOD[];
     res = res.filter(r => {
-      r.src = r.hdurl || r.url;
+      r.src = r.url || r.hdurl;
       r.alt = r.explanation;
       return r.media_type === "image";
     }).reverse();
@@ -124,8 +124,8 @@ onMounted(async () => {
         <img src="/loading.svg" alt="loading icon" />
       </div>
       <Carousel v-else :slides-array="slides" :transition-speed="1" :auto-slide="false" :items-to-show="4"
-        :breakpoints="breakpoints" slide-container-class="slideContainer" next-btn-class="slide-btn"
-        prev-btn-class="slide-btn" @img-clicked="viewImgDetails">
+        :breakpoints="breakpoints" slide-container-class="slideContainer" next-btn-class="slide-btn --next"
+        prev-btn-class="slide-btn --prev" @img-clicked="viewImgDetails">
       </Carousel>
     </section>
     <section class="details-section">
@@ -144,27 +144,69 @@ onMounted(async () => {
   </main>
 </template>
 
-<style>
+<style lang="scss">
+$screen-width-xxl: 1124px;
+$screen-width-xl: 900px;
+$screen-width-l: 729px;
+$screen-width-m: 549px;
+$screen-width-s: 416px;
+$screen-width-xs: 365px;
+
+$section-padding: 16px;
+
+$details-img-xxl: 500px;
+$details-img-xl: 400px;
+$details-img-l: 300px;
+$details-img-m: 260px;
+$details-img-s: 200px;
+$details-img-xs: 160px;
+
+$border-radius: 8px;
+
+:root {
+  --details-img-size: #{$details-img-xxl};
+}
+
 .container {
   height: 100%;
 }
 
 .carousel-section {
-  background-color: black;
-  height: 40%;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: $section-padding 0;
+  background-color: #303030;
 }
 
 .loading {
   height: 100px;
   width: 100px;
-}
 
-.loading img {
-  width: 100%;
-  height: 100%;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (max-width: $screen-width-l) and (min-width: calc($screen-width-m + 1px)) {
+    height: 75px;
+    width: 75px;
+  }
+
+  @media (max-width: $screen-width-m) and (min-width: calc($screen-width-s + 1px)) {
+    height: 75px;
+    width: 75px;
+  }
+
+  @media (max-width: $screen-width-s) and (min-width: calc($screen-width-xs + 1px)) {
+    height: 50px;
+    width: 50px;
+  }
+
+  @media (max-width: $screen-width-xs) {
+    height: 50px;
+    width: 50px;
+  }
 }
 
 .carousel__container {
@@ -173,284 +215,121 @@ onMounted(async () => {
 
 
 .slideContainer * {
-  padding: 2px;
-  background-color: #f0e68c;
   cursor: pointer;
-  border-radius: 10px;
+  border-radius: $border-radius;
   border: 0;
+  /* box-shadow: 0px 0px 2px 0px #f0e68c; */
+}
+
+.slide-btn {
+  background-color: #3b3b3b !important;
+  border: 0;
+
+  &.--next {
+    border-top-left-radius: $border-radius;
+    border-bottom-left-radius: $border-radius;
+  }
+
+  &.--prev {
+    border-top-right-radius: $border-radius;
+    border-bottom-right-radius: $border-radius;
+  }
+
+  &:hover {
+    background-color: #373737 !important;
+  }
 }
 
 .details-section {
-  background-color: #303030;
-  height: 60%;
   display: flex;
   justify-content: flex-start;
-  align-items: center;
+  align-items: flex-start;
   color: #ede8bf;
+
+  @media (max-width: $screen-width-l) {
+    justify-content: center;
+  }
 }
 
 .details-container {
   display: flex;
-  padding: 20px;
+  padding: $section-padding;
+
+  @media (max-width: $screen-width-l) {
+    flex-direction: column;
+    align-items: center;
+  }
 }
 
+
+
 .img-container {
-  min-width: 450px;
-  width: 450px;
-  min-height: 450px;
-  height: 450px;
-  margin-right: 20px;
+  min-width: var(--details-img-size);
+  width: var(--details-img-size);
+  min-height: var(--details-img-size);
+  height: var(--details-img-size);
+  margin-right: $section-padding;
 
   img {
-    padding: 2px;
-    background-color: #f0e68c;
-    border-radius: 10px;
+    border-radius: $border-radius;
     border: 0;
     width: 100%;
     height: 100%;
+    /* box-shadow: 0px 0px 2px 0px #f0e68c; */
+  }
+
+  @media (max-width: $screen-width-xxl) and (min-width: calc($screen-width-xl + 1px)) {
+    --details-img-size: #{$details-img-xl};
+  }
+
+  @media (max-width: $screen-width-xl) and (min-width: calc($screen-width-l + 1px)) {
+    --details-img-size: #{$details-img-l};
+  }
+
+  @media (max-width: $screen-width-l) and (min-width: calc($screen-width-m + 1px)) {
+    --details-img-size: #{$details-img-m};
+  }
+
+  @media (max-width: $screen-width-m) and (min-width: calc($screen-width-s + 1px)) {
+    --details-img-size: #{$details-img-s};
+  }
+
+  @media (max-width: $screen-width-s) {
+    --details-img-size: #{$details-img-xs};
+  }
+
+  @media (max-height: 850px) and (min-width: calc($screen-width-xl + 1px)) {
+    --details-img-size: calc(#{$details-img-xxl} - 50px);
   }
 }
 
 .img-desc {
-  height: 450px;
-}
-
-.img-desc .__title {
-  color: #f0e68c;
-}
-
-.img-desc .__desc {
-  height: 70%;
-  color: #b1b1b1;
-  /* max-width: 60ch; */
-  overflow: auto;
-  background-color: #2d2d2a8f;
-  border: 0;
-  border-radius: 10px;
-  padding: 10px;
-  padding-right: 6px;
-  scrollbar-color: #616161 #2d2d2a8f;
-  scrollbar-width: thin;
-}
-
-@media (max-width: 1124px) and (min-width: 901px) {
-  .carousel-section {
-    height: 30%;
+  .__title {
+    color: #f0e68c;
+    padding: $section-padding;
+    margin: 0;
   }
 
-  .details-section {
-    height: 70%;
+  .__desc {
+    box-sizing: border-box;
+    max-height: calc(var(--details-img-size) / 2.5);
+    color: #b1b1b1;
+    /* max-width: 60ch; */
+    overflow: auto;
+    background-color: #2d2d2a8f;
+    border: 0;
+    border-radius: $border-radius;
+    padding: $section-padding;
+    padding-right: 6px;
+    scrollbar-color: #616161 #2d2d2a8f;
+    scrollbar-width: thin;
   }
 
-  .img-container {
-    min-width: 550px;
-    width: 550px;
-    min-height: 550px;
-    height: 550px;
-  }
-
-  .img-desc {
-    height: 550px;
-  }
-}
-
-@media (max-width: 900px) and (min-width: 831px) {
-  .carousel-section {
-    height: 30%;
-  }
-
-  .details-section {
-    height: 70%;
-  }
-
-  .img-container {
-    min-width: 500px;
-    width: 500px;
-    min-height: 500px;
-    height: 500px;
-  }
-
-  .img-desc {
-    height: 500px;
-  }
-}
-
-@media (max-width: 830px) and (min-width: 791px) {
-  .carousel-section {
-    height: 30%;
-  }
-
-  .details-section {
-    height: 70%;
-  }
-
-  .img-container {
-    min-width: 450px;
-    width: 450px;
-    min-height: 450px;
-    height: 450px;
-  }
-
-  .img-desc {
-    height: 450px;
-  }
-}
-
-@media (max-width: 790px) and (min-width: 730px) {
-  .carousel-section {
-    height: 30%;
-  }
-
-  .details-section {
-    height: 70%;
-  }
-
-  .img-container {
-    min-width: 400px;
-    width: 400px;
-    min-height: 400px;
-    height: 400px;
-  }
-
-  .img-desc {
-    height: 400px;
-  }
-}
-
-@media (max-width: 729px) and (min-width: 550px) {
-  .carousel-section {
-    height: 20%;
-  }
-
-  .details-section {
-    height: 80%;
-    justify-content: center;
-    align-items: baseline;
-  }
-
-  .details-container {
-    flex-direction: column;
-    justify-content: center;
+  @media (max-width: $screen-width-l) {
+    display: flex;
+    justify-content: flex-start;
     align-items: center;
-  }
-
-  .loading {
-    height: 75px;
-    width: 75px;
-  }
-
-  .img-container {
-    min-width: 400px;
-    width: 400px;
-    min-height: 400px;
-    height: 400px;
-  }
-
-  .img-desc {
-    height: 200px;
-  }
-}
-
-
-@media (max-width: 549px) and (min-width: 415px) {
-  .carousel-section {
-    height: 20%;
-  }
-
-  .details-section {
-    height: 80%;
-    justify-content: center;
-    align-items: baseline;
-  }
-
-  .details-container {
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .loading {
-    height: 75px;
-    width: 75px;
-  }
-
-  .img-container {
-    min-width: 350px;
-    width: 350px;
-    min-height: 350px;
-    height: 350px;
-  }
-
-  .img-desc {
-    height: 175px;
-  }
-}
-
-@media (max-width: 416px) and (min-width: 366px) {
-  .carousel-section {
-    height: 20%;
-  }
-
-  .details-section {
-    height: 80%;
-    justify-content: center;
-    align-items: baseline;
-  }
-
-  .details-container {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .loading {
-    height: 50px;
-    width: 50px;
-  }
-
-  .img-container {
-    min-width: 300px;
-    width: 300px;
-    min-height: 300px;
-    height: 300px;
-  }
-
-  .img-desc {
-    height: 200px;
-  }
-}
-
-@media (max-width: 365px) {
-  .carousel-section {
-    height: 20%;
-  }
-
-  .details-section {
-    height: 80%;
-    justify-content: center;
-    align-items: baseline;
-  }
-
-  .details-container {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .loading {
-    height: 50px;
-    width: 50px;
-  }
-
-  .img-container {
-    min-width: 250px;
-    width: 250px;
-    min-height: 250px;
-    height: 250px;
-  }
-
-  .img-desc {
-    height: 250px;
   }
 }
 </style>
